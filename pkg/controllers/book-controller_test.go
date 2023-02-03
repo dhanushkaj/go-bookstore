ea59ba3c90d72e24ea496877589f4c6df20a9e76
dhanushka jayathilaka <djayathilaka@kaleris.com>
2023-02-03 21:31:53 +0530

    added test cases

--- pkg/controllers/book-controller_test.go
+++ pkg/controllers/book-controller_test.go
@@ -17,6 +17,83 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+type TestBook struct {
+	ID uint
+}
+
+var testBook TestBook
+
+func TestCreateBook(t *testing.T) {
+
+	router := mux.NewRouter()
+	router.HandleFunc("/book", utils.MakeHttpHandler(controllers.CreateBook)).Methods("POST")
+	r, _ := repository.InitDB()
+	models.InitBook(r.DB)
+
+	var book models.Book
+
+	name := "Test Name2"
+	author := "Test Author2"
+	publication := "Test Publication2"
+
+	book.Name = &name
+	book.Author = &author
+	book.Publication = &publication
+
+	postBook, _ := json.Marshal(book)
+	reader := bytes.NewReader(postBook)
+
+	req := httptest.NewRequest(http.MethodPost, "/book", reader)
+	w := httptest.NewRecorder()
+	router.ServeHTTP(w, req)
+
+	resp := w.Result()
+	body, _ := ioutil.ReadAll(resp.Body)
+	defer resp.Body.Close()
+	t.Log(string(body))
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+
+	var responseBook models.Book
+	err := json.Unmarshal(body, &responseBook)
+	responseName := *responseBook.Name
+	testBook.ID = *&responseBook.ID
+	assert.Nil(t, err)
+	assert.Equal(t, name, responseName)
+
+}
+
+func TestUpdateBook(t *testing.T) {
+
+	router := mux.NewRouter()
+	router.HandleFunc("/book/{bookId}", utils.MakeHttpHandler(controllers.UpdateBook)).Methods("PUT")
+	r, _ := repository.InitDB()
+	models.InitBook(r.DB)
+
+	var book models.Book
+
+	name := "Test Name3"
+	author := "Test Author3"
+	publication := "Test Publication3"
+
+	book.Name = &name
+	book.Author = &author
+	book.Publication = &publication
+
+	postBook, _ := json.Marshal(book)
+	reader := bytes.NewReader(postBook)
+
+	req := httptest.NewRequest(http.MethodPut, "/book/"+fmt.Sprintf("%v", testBook.ID), reader)
+	w := httptest.NewRecorder()
+	router.ServeHTTP(w, req)
+
+	resp := w.Result()
+	body, _ := ioutil.ReadAll(resp.Body)
+	defer resp.Body.Close()
+	t.Log(string(body))
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+
+}
+
 func TestGetAllBooks(t *testing.T) {
 	router := mux.NewRouter()
 	router.HandleFunc("/books", utils.MakeHttpHandler(controllers.GetAllBooks)).Methods("GET")
@@ -46,7 +123,7 @@ func TestBookById(t *testing.T) {
 	r, _ := repository.InitDB()
 	models.InitBook(r.DB)
 
-	req := httptest.NewRequest(http.MethodGet, "/book/3", nil)
+	req := httptest.NewRequest(http.MethodGet, "/book/"+fmt.Sprintf("%v", testBook.ID), nil)
 	w := httptest.NewRecorder()
 	router.ServeHTTP(w, req)
 
@@ -62,7 +139,7 @@ func TestBookById(t *testing.T) {
 	id := &book.ID
 
 	assert.Nil(t, err)
-	assert.Equal(t, *id, uint(3))
+	assert.Equal(t, *id, uint(testBook.ID))
 
 }
 
@@ -73,39 +150,7 @@ func TestDeleteBook(t *testing.T) {
 	r, _ := repository.InitDB()
 	models.InitBook(r.DB)
 
-	req := httptest.NewRequest(http.MethodDelete, "/book/3", nil)
-	w := httptest.NewRecorder()
-	router.ServeHTTP(w, req)
-
-	resp := w.Result()
-	body, _ := ioutil.ReadAll(resp.Body)
-	defer resp.Body.Close()
-	t.Log(string(body))
-	assert.Equal(t, http.StatusOK, resp.StatusCode)
-
-}
-
-func TestUpdateBook(t *testing.T) {
-
-	router := mux.NewRouter()
-	router.HandleFunc("/book/{bookId}", utils.MakeHttpHandler(controllers.UpdateBook)).Methods("PUT")
-	r, _ := repository.InitDB()
-	models.InitBook(r.DB)
-
-	var book models.Book
-
-	name := "Test Name"
-	author := "Test Author"
-	publication := "Test Publication"
-
-	book.Name = &name
-	book.Author = &author
-	book.Publication = &publication
-
-	postBook, _ := json.Marshal(book)
-	reader := bytes.NewReader(postBook)
-
-	req := httptest.NewRequest(http.MethodPut, "/book/3", reader)
+	req := httptest.NewRequest(http.MethodDelete, "/book/"+fmt.Sprintf("%v", testBook.ID), nil)
 	w := httptest.NewRecorder()
 	router.ServeHTTP(w, req)
 
